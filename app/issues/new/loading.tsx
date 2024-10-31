241a1e19df09d4a7d7637875c87043b74924f5d9
adnan4k <fayomuhe5@gmail.com>
2024-10-31 21:27:40 +0300

    additional loading skeleton added

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
-function newIssueLoading() {
+import { Box } from '@radix-ui/themes';
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const LoadingNewIssuePage = () => {
   return (
-    <div>newIssueLoading...</div>
-  )
-}
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+    </Box>
+  );
+};
 
-export default newIssueLoading
+export default LoadingNewIssuePage;
